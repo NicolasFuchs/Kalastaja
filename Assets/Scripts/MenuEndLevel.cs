bde1da041f85a314e3e6dc1f0b3c09074e0e537d
Jige <jige.pont@gmail.com>
2018-04-23 16:36:25 +0200

    Initial with MainMenu and MenuEndLevel

---
+++ Assets/Scripts/MenuEndLevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuEndLevel : MonoBehaviour {
+    public Button btnQuit;
+    public Button btnContinue;
+
+    // Use this for initialization
+    void Start()
+    {
+        btnQuit.GetComponent<Button>().onClick.AddListener(OnQuit);
+        btnContinue.GetComponent<Button>().onClick.AddListener(OnContinue);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnQuit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                         Application.Quit();
+        #endif
+    }
+
+    void OnContinue()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+}
---
+++ Assets/Scripts/MenuMain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuMain : MonoBehaviour {
+    public Button btnQuit;
+    public Button btnStart;
+
+    // Use this for initialization
+    void Start () {
+        btnQuit.GetComponent<Button>().onClick.AddListener(OnQuit);
+        btnStart.GetComponent<Button>().onClick.AddListener(OnStart);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnQuit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                 Application.Quit();
+        #endif
+    }
+
+    void OnStart()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+}
